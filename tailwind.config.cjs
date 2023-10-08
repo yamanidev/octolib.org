4837fb8e6fb334a238a2fb057deff5ccfee5c450
yamanidev <elamine.yamani@gmail.com>
2023-10-08 13:41:42 +0100

    Added configuration

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,24 @@
 /** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        beige: '#FAEBD9',
+        'blue-light': '#2B9AAE',
+        'blue-dark': '#1B5F6A',
+        sand: '#D4A040'
+      },
+      fontFamily: {
+        montserrat: ['Montserrat', 'sans-serif']
+      },
+      boxShadow: {
+        navbar: '0px 4px 10px 0px rgba(0, 0, 0, 0.25)'
+      }
+    }
   },
-  plugins: [],
+  plugins: []
 };
