64d26c4e2ad58cd0b93f0605cf123e454cc58b88
yamanidev <elamine.yamani@gmail.com>
2023-10-10 12:42:25 +0100

    Compiled specialties in one file

---
+++ src/data/fourthYear.ts
@@ -0,0 +1,212 @@
+import type { Specialty } from './types';
+
+const fourthYear: Specialty = {
+  iasd: {
+    semester1: [
+      {
+        title: 'Operating Systems 1',
+        link: '#'
+      },
+      {
+        title: 'Networking 1',
+        link: '#'
+      },
+      {
+        title: 'Introduction to Software Engineering',
+        link: '#'
+      },
+      {
+        title: 'Databases',
+        link: '#'
+      },
+      {
+        title: 'Language Theory',
+        link: '#'
+      },
+      {
+        title: 'Numerical Analysis',
+        link: '#'
+      },
+      {
+        title: 'Operational Research',
+        link: '#'
+      },
+      {
+        title: 'English Language 1',
+        link: '#'
+      }
+    ],
+    semester2: [
+      {
+        title: 'Operating Systems 2',
+        link: '#'
+      },
+      {
+        title: 'Networking 2',
+        link: '#'
+      },
+      {
+        title: 'Advanced Computer Architecture',
+        link: '#'
+      },
+      {
+        title: 'Analysis and Design of Information Systems',
+        link: '#'
+      },
+      {
+        title: 'Web Languages and Tools',
+        link: '#'
+      },
+      {
+        title: 'Introduction to Computer Security',
+        link: '#'
+      },
+      {
+        title: 'Project Management',
+        link: '#'
+      },
+      {
+        title: 'English Language 2',
+        link: '#'
+      }
+    ]
+  },
+  isi: {
+    semester1: [
+      {
+        title: 'Embedded Systems 1',
+        link: '#'
+      },
+      {
+        title: 'Advanced Networking',
+        link: '#'
+      },
+      {
+        title: 'Advanced Databases',
+        link: '#'
+      },
+      {
+        title: 'Analysis and Design of Advanced Algorithms',
+        link: '#'
+      },
+      {
+        title: 'Human-Computer Interaction',
+        link: '#'
+      },
+      {
+        title: 'Knowledge Engineering',
+        link: '#'
+      },
+      {
+        title: 'Decision Support Systems',
+        link: '#'
+      },
+      {
+        title: 'Web Technologies and Development',
+        link: '#'
+      }
+    ],
+    semester2: [
+      {
+        title: 'Distributed Systems',
+        link: '#'
+      },
+      {
+        title: 'Systems and Network Security',
+        link: '#'
+      },
+      {
+        title: 'Embedded Systems 2',
+        link: '#'
+      },
+      {
+        title: 'Mobile Technologies and Development',
+        link: '#'
+      },
+      {
+        title: 'Modeling and Simulation',
+        link: '#'
+      },
+      {
+        title: 'Data Analysis',
+        link: '#'
+      },
+      {
+        title: 'Multimedia Systems',
+        link: '#'
+      }
+    ]
+  },
+  siw: {
+    semester1: [
+      {
+        title: 'Operating Systems 1',
+        link: '#'
+      },
+      {
+        title: 'Networking 1',
+        link: '#'
+      },
+      {
+        title: 'Introduction to Software Engineering',
+        link: '#'
+      },
+      {
+        title: 'Databases',
+        link: '#'
+      },
+      {
+        title: 'Language Theory',
+        link: '#'
+      },
+      {
+        title: 'Numerical Analysis',
+        link: '#'
+      },
+      {
+        title: 'Operational Research',
+        link: '#'
+      },
+      {
+        title: 'English Language 1',
+        link: '#'
+      }
+    ],
+    semester2: [
+      {
+        title: 'Operating Systems 2',
+        link: '#'
+      },
+      {
+        title: 'Networking 2',
+        link: '#'
+      },
+      {
+        title: 'Advanced Computer Architecture',
+        link: '#'
+      },
+      {
+        title: 'Analysis and Design of Information Systems',
+        link: '#'
+      },
+      {
+        title: 'Web Languages and Tools',
+        link: '#'
+      },
+      {
+        title: 'Introduction to Computer Security',
+        link: '#'
+      },
+      {
+        title: 'Project Management',
+        link: '#'
+      },
+      {
+        title: 'English Language 2',
+        link: '#'
+      }
+    ]
+  }
+};
+
+export default fourthYear;
