7211a5b83a386b2c647c41cac1f0d978dd736b79
yamanidev <elamine.yamani@gmail.com>
2023-10-11 23:11:03 +0100

    Added second semester links

--- src/data/firstYear.ts
+++ src/data/firstYear.ts
@@ -38,27 +38,27 @@ const firstYearCurriculum: Curriculum = {
   semester2: [
     {
       title: 'Algorithms and Dynamic Data Structures',
-      link: '#'
+      link: 'https://drive.google.com/drive/folders/154937lMeCeaxflpU7SlbTvx3NZV6NDUT'
     },
     {
       title: 'Introduction to Operating Systems 2',
-      link: '#'
+      link: 'https://drive.google.com/drive/folders/1CD9AMbiiRBUI21B043lvDm5aehtxRoSn'
     },
     {
       title: 'Mathematical Analysis 2',
-      link: '#'
+      link: 'https://drive.google.com/drive/folders/1KNGS0bNQ0xWUm8_5_5uNnQwVhxeL7q5D'
     },
     {
       title: 'Algebra 2',
-      link: '#'
+      link: 'https://drive.google.com/drive/folders/1PBcK2wXwNwOjyZC5U5znHDEtDdqeK3OM'
     },
     {
       title: 'Point Mechanics',
-      link: '#'
+      link: 'https://drive.google.com/drive/folders/1Ye1cI2aXVtSS8gQIEM-sxZhgjtROYpin'
     },
     {
       title: 'Fundamental Electronics 1',
-      link: '#'
+      link: 'https://drive.google.com/drive/folders/1RXl_BXdqICfHofZIQ_QyUtzeIjFd6gwJ'
     },
     {
       title: 'Oral Expression Technique',
@@ -66,7 +66,7 @@ const firstYearCurriculum: Curriculum = {
     },
     {
       title: 'English Language 1',
-      link: '#'
+      link: 'https://drive.google.com/drive/folders/1W3WSY_HG76eq0t0La3506TmgzNo0SUTn'
     }
   ]
 };
