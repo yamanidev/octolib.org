794042f4cef28fcd12cb70fff143e15972ea8b6a
yamanidev <elamine.yamani@gmail.com>
2023-10-11 10:21:41 +0100

    Added Preact

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from '@astrojs/tailwind';
+
+import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind(), preact()]
 });
