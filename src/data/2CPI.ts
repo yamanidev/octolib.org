252f4db80dab4d1e6f7bc7eef6534c83f77e895e
yamanidev <elamine.yamani@gmail.com>
2023-10-17 12:05:19 +0100

    Added new links

--- src/data/2CPI.ts
+++ src/data/2CPI.ts
@@ -4,65 +4,65 @@ const secondYearCurriculum: Curriculum = {
   semester1: [
     {
       title: 'File and Data Structures',
-      link: 'https://drive.google.com/drive/folders/1IWOjQBM0eBMkH-9Gy11rQcZSMomQEega'
+      link: 'https://drive.google.com/drive/folders/1Mrw3oq_fzIkyks3Y_74ZQVYPo3i7IKLi?usp=drive_link'
     },
     {
       title: 'Computer Architecture 2',
-      link: 'https://drive.google.com/drive/folders/15bCBZK9mo3OVHKmJk06iw_eViAazy7LJ'
+      link: 'https://drive.google.com/drive/folders/19QrlJHPSxbbSbg1cBMdI72OVZkOw8hzT?usp=drive_link'
     },
     {
       title: 'Mathematical Analysis 3',
-      link: 'https://drive.google.com/drive/folders/1dGbU14LEC68xPYJI-pc5UBZSOwiK8e8h'
+      link: 'https://drive.google.com/drive/folders/1CjCpTmJ6J8zxBPH6kPH26czmelDj1kdY?usp=drive_link'
     },
     {
       title: 'Algebra 3',
-      link: 'https://drive.google.com/drive/folders/13sBrfzPYl7cWxcU_JDpP45CtQXxusnU4'
+      link: 'https://drive.google.com/drive/folders/1Fw6eIHZ-Ai-2LBwqjxVget-bieuMjDGV?usp=drive_link'
     },
     {
       title: 'Fundamental Electronics 2',
-      link: 'https://drive.google.com/drive/folders/1ka45Tv6vE-sEd0Zw2f9J84CyPq_F7WYu'
+      link: 'https://drive.google.com/drive/folders/1dUtwissojIXMRopHVPS1oySATFsClFaL?usp=drive_link'
     },
     {
       title: 'Probability and Statistics 1',
-      link: 'https://drive.google.com/drive/folders/1fha4zXPlDwQEt_uK_0KQnZ0YtDVcGkda'
+      link: 'https://drive.google.com/drive/folders/1S6lcOq2wwon3c27wIw3kDft-2DzbXBvf?usp=drive_link'
     },
     {
       title: 'English Language 2',
-      link: 'https://drive.google.com/drive/folders/13nEVxVtvx8t9ClrWHb5iUTv2FA0bFLkj'
+      link: 'https://drive.google.com/drive/folders/1yEUZmrGnqOk_ZCJqIg168TPrztXkPzDP?usp=drive_link'
     },
     {
       title: 'Business Economics',
-      link: 'https://drive.google.com/drive/folders/1Ph5YKo2LBK9yu8LNpCt8O8juPinhXkmm'
+      link: 'https://drive.google.com/drive/folders/11HVlIlGtVvnwYAzgY7lPEGqsfg16u7ZM?usp=drive_link'
     }
   ],
   semester2: [
     {
       title: 'Object Oriented Programming',
-      link: 'https://drive.google.com/drive/folders/1mIOlacQWhX9iC6Dp0kMWE1PLVxco1oto'
+      link: 'https://drive.google.com/drive/folders/1n4clGg7y8zEGGBg-9V_omY9KRzSeW-5s?usp=drive_link'
     },
     {
       title: 'Introduction to Information Systems',
-      link: 'https://drive.google.com/drive/folders/19oI8MB8wCyf173lZE2aBzyNKAor1Q1u2'
+      link: 'https://drive.google.com/drive/folders/1I2TIBmzAY0mqh3ghJzUqvWGl7RuruxSB?usp=drive_link'
     },
     {
       title: 'Mathematical Analysis 4',
-      link: 'https://drive.google.com/drive/folders/18OK1wi24FzSQuaOYwpUJVt5wenPeWKT1'
+      link: 'https://drive.google.com/drive/folders/1EvOHzrqg7lFujqDJzNmdI-uHb8WWJLCZ?usp=drive_link'
     },
     {
       title: 'Mathematical Logic',
-      link: 'https://drive.google.com/drive/folders/1D4L4KXS3aZ_-ZkZCk7_nfE0X3gy0wPbq'
+      link: 'https://drive.google.com/drive/folders/1wQ6BkTKKBOcXBp6WjtwiNvjYgih27cj9?usp=drive_link'
     },
     {
       title: 'Optics and Electromagnetic Waves',
-      link: 'https://drive.google.com/drive/folders/14PkJ7DPhRowa-ngNCC3D2ciEjPKv-ZZk'
+      link: 'https://drive.google.com/drive/folders/1xV3VzdaIfPpw8uUybIBE0kXFNW3K6URB?usp=drive_link'
     },
     {
       title: 'Probability and Statistics 2',
-      link: 'https://drive.google.com/drive/folders/1uU4MORfOXm3PcJrQ5BSIRWOjYMpDpQuL'
+      link: 'https://drive.google.com/drive/folders/1dFzZrJE7tkwWrJvB6rlzeNirZyGu5Cm3?usp=drive_link'
     },
     {
       title: 'English Language 3',
-      link: 'https://drive.google.com/drive/folders/1jQ2ARe-EaT3qmKDhDEKJU2oNxjLhtA-I'
+      link: 'https://drive.google.com/drive/folders/1Ezwi-_R1VlycJuTIeh6JGNxMdJDF_joY?usp=drive_link'
     }
   ]
 };
