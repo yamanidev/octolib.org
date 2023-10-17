40c2291afcbf3fc35e7c57d51c7821f7fdf2b6b2
yamanidev <elamine.yamani@gmail.com>
2023-10-17 12:20:19 +0100

    Added new links

--- src/data/2CS.ts
+++ src/data/2CS.ts
@@ -5,65 +5,65 @@ const fourthYear: Specialty = {
     semester1: [
       {
         title: 'Embedded Systems 1',
-        link: 'https://drive.google.com/drive/folders/1dmHuaqBR_OO5Rqc5mzvRSTG4oZhffEqh'
+        link: 'https://drive.google.com/drive/folders/15hE3EDCDPIFpX7KvmhSwAuJZXIHRM1uE?usp=drive_link'
       },
       {
         title: 'Advanced Networking',
-        link: 'https://drive.google.com/drive/folders/1W-TlA16jx7X6o14-6krZ5nlrQwlioa92'
+        link: 'https://drive.google.com/drive/folders/1jfWqo43l31bbGq-71Lx4PVon4B-CB-67?usp=drive_link'
       },
       {
         title: 'Advanced Databases',
-        link: 'https://drive.google.com/drive/folders/1AnLQx4X7GJnf_BR45gBqc4cPNTnf2icC'
+        link: 'https://drive.google.com/drive/folders/1WZCOWbujJnu-Rhx86ivUiyc6Ue7fSAML?usp=drive_link'
       },
       {
         title: 'Analysis and Design of Advanced Algorithms',
-        link: 'https://drive.google.com/drive/folders/178cMHzTn3v6oI-kSOVdfDer_lnUIK_N0'
+        link: 'https://drive.google.com/drive/folders/178FdznCwIvKa3j7-PTTJx9ZZMKC8YhZX?usp=drive_link'
       },
       {
         title: 'Human-Computer Interaction',
-        link: 'https://drive.google.com/drive/folders/1DmaAyiK6i_qOYMzfPuKWARA3dAM2AiYf'
+        link: 'https://drive.google.com/drive/folders/1b4AJtvr_h2zQNwsOfN9dhm36E04mKkUP?usp=drive_link'
       },
       {
         title: 'Knowledge Engineering',
-        link: 'https://drive.google.com/drive/folders/19wVaXhPpKSwA8WPBcIHCTJrdKpNseIlW'
+        link: 'https://drive.google.com/drive/folders/1nNMmuzwVeAdD1UeETRm8IN0rTKC9eKV1?usp=drive_link'
       },
       {
         title: 'Decision Support Systems',
-        link: 'https://drive.google.com/drive/folders/1jYq5lqgAon0crfPbmyywQ6AXi4AREenx'
+        link: 'https://drive.google.com/drive/folders/1IdumPeKK5w0LdTy3aUuneZTMzR90vOnI?usp=drive_link'
       },
       {
         title: 'Web Technologies and Development',
-        link: 'https://drive.google.com/drive/folders/1aTQBTfL3gFoYBtDp7eDfT3UrMZSvxPXI'
+        link: 'https://drive.google.com/drive/folders/1BmHV93ASP-uMlf9CkkLZoYYNgqNz2AZV?usp=drive_link'
       }
     ],
     semester2: [
       {
         title: 'Distributed Systems',
-        link: 'https://drive.google.com/drive/folders/1iL81rKoKB8NKHnhjVeB6xSxdW0fdznmm'
+        link: 'https://drive.google.com/drive/folders/146_3SKv-FFYvmbyECBYglHRPGPMgGriE?usp=drive_link'
       },
       {
         title: 'Systems and Network Security',
-        link: 'https://drive.google.com/drive/folders/1iHfZ15JgSyqCCTyG7VTWNrPXgaLUVODF'
+        link: 'https://drive.google.com/drive/folders/1QDmqRJG5_tanKnu7GCYzVUbrcOlrxCfN?usp=drive_link'
       },
       {
         title: 'Embedded Systems 2',
-        link: 'https://drive.google.com/drive/folders/1-cyuYXkH4odcIaZ0nyOnWpl8n8cowZ8u'
+        link: 'https://drive.google.com/drive/folders/1XoKVCOu51Xdoi_a6QfmURzLn87WNdVa3?usp=drive_link'
       },
       {
         title: 'Mobile Technologies and Development',
-        link: 'https://drive.google.com/drive/folders/1hxCStv4BMqhnp1wBsa4SD2Nx_FFv3xuK'
+        link: 'https://drive.google.com/drive/folders/1tFTGMPZwDuwaXjJnN-zvxGHvHLGPbBYF?usp=drive_link'
       },
       {
         title: 'Modeling and Simulation',
-        link: 'https://drive.google.com/drive/folders/1eAUXadG03g4Mde6RLbNvDAHFjv4ZwK6v'
+        link: 'https://drive.google.com/drive/folders/1298EWpUbWedkB2zKXWPyz5bbrwAd4GYL?usp=drive_link'
       },
       {
         title: 'Data Analysis',
-        link: 'https://drive.google.com/drive/folders/1W06i3C7FFYawuKHjOjRwgX3z-sEKrhxP'
+        link: 'https://drive.google.com/drive/folders/1SWhcQrBjNGDItd8oF2dRiODumNnoC1pq?usp=drive_link'
       },
       {
         title: 'Multimedia Systems',
-        link: 'https://drive.google.com/drive/folders/15_WoSSo4_InXNTCIPvqYYTnhV2DmE_sd'
+        link: 'https://drive.google.com/drive/folders/19S7AB2U5iZXh7u4n-5jFJTHfH9DQseRc?usp=drive_link'
       }
     ]
   },
@@ -141,69 +141,69 @@ const fourthYear: Specialty = {
     semester1: [
       {
         title: 'Advanced Databases',
-        link: '#'
+        link: 'https://drive.google.com/drive/folders/1IBZH52KQZUspTe_oKLkOHthKDfLciMPI?usp=drive_link'
       },
       {
         title: 'Machine Learning',
-        link: '#'
+        link: 'https://drive.google.com/drive/folders/1q5BrKF7Fb7OqCw_GtXo-lTH8PY2buDI9?usp=drive_link'
       },
       {
         title: 'Software Engineering for Data Science',
-        link: '#'
+        link: 'https://drive.google.com/drive/folders/1N9cG6LlFbYFDlyJ_bQK-a3J4zx2diHD3?usp=drive_link'
       },
       {
         title: 'Knowledge Engineering',
-        link: '#'
+        link: 'https://drive.google.com/drive/folders/1tQ142ha0BGJ18pYASFaWe1cVW5s85SKW?usp=drive_link'
       },
       {
         title: 'Advanced Mathematics for Data Science',
-        link: '#'
+        link: 'https://drive.google.com/drive/folders/1IPwCQL58B9vzVIppYSoYuisxcKS5SzJm?usp=drive_link'
       },
       {
         title: 'Complexity and Problem Solving',
-        link: '#'
+        link: 'https://drive.google.com/drive/folders/115f-17CiUfScy2TaiC6JLPFSJ4gxPJad?usp=drive_link'
       },
       {
         title: 'Human-Computer Interaction',
-        link: '#'
+        link: 'https://drive.google.com/drive/folders/1PbZIgdsRk0hkVvCfegLxOwkdKmcYzesu?usp=drive_link'
       },
       {
         title: 'Advanced Networking',
-        link: '#'
+        link: 'https://drive.google.com/drive/folders/1jmKRROVPiaviwnYQr14--jr94y1zSuv5?usp=drive_link'
       }
     ],
     semester2: [
       {
         title: 'Deep Learning',
-        link: '#'
+        link: 'https://drive.google.com/drive/folders/1LBPPf8RbLT_Si7lYpbMwBHcc9HXVn-p6?usp=drive_link'
       },
       {
         title: 'Natural Language Processing',
-        link: '#'
+        link: 'https://drive.google.com/drive/folders/1CiV3IHwLTXDc0mZJJuB6HFJ2O3zEtCUh?usp=drive_link'
       },
       {
         title: 'High Performance Computing',
-        link: '#'
+        link: 'https://drive.google.com/drive/folders/139xvyV1U-AI5bB8_kOERfLppT81jnqEk?usp=drive_link'
       },
       {
         title: 'Big Data Technologies',
-        link: '#'
+        link: 'https://drive.google.com/drive/folders/1uwBslHR2c_N3e6eAivw5TmYwngCwCBKW?usp=drive_link'
       },
       {
         title: 'IT Security',
-        link: '#'
+        link: 'https://drive.google.com/drive/folders/1ZeKx-W-qEaBk8vAKyOcsWXmI7AZ7zuWy?usp=drive_link'
       },
       {
         title: 'Modeling and Simulation',
-        link: '#'
+        link: 'https://drive.google.com/drive/folders/1ebUYrjz6phtS5CW1D1vq5xjDzvy23F0o?usp=drive_link'
       },
       {
         title: 'Data Analysis',
-        link: '#'
+        link: 'https://drive.google.com/drive/folders/1zTK9RbdGz9Ie1T8-a8LCyOkUKwF_KeFn?usp=drive_link'
       },
       {
         title: 'Virtual Reality and Augmented Reality',
-        link: '#'
+        link: 'https://drive.google.com/drive/folders/1cej_o8WmY__tPPSziB-fVAKu9vbTNhQO?usp=drive_link'
       }
     ]
   }
