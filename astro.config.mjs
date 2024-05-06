b43b50dc1f7b643132499e5b65f5960b1e399bb2
yamanidev <elamine.yamani@gmail.com>
2024-05-06 19:22:49 +0100

    Added pretty script + formatted project

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
-import preact from "@astrojs/preact";
+import preact from '@astrojs/preact';
 
 // https://astro.build/config
 export default defineConfig({
