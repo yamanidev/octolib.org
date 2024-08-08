2a5971c44f4c3c29403b60c8fcc67d1ec6129281
yamanidev <elamine.yamani@gmail.com>
2024-08-08 11:32:53 +0100

    Removed Preact dependency and changed TS to strict

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,8 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
-import preact from "@astrojs/preact";
-
 // https://astro.build/config
 export default defineConfig({
   site: "https://octolib.pages.dev/",
-  integrations: [tailwind(), preact()]
+  integrations: [tailwind()]
 });
