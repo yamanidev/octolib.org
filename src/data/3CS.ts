7f013f5d1aa1aafb7caf5af8d3435d8c0ec8e65c
yamanidev <elamine.yamani@gmail.com>
2024-08-08 19:30:57 +0100

    added 3CS links

---
+++ src/data/3CS.ts
@@ -0,0 +1,121 @@
+import type { BranchedCurriculum } from "./types";
+
+const fifthYear: BranchedCurriculum = {
+  ISI: {
+    semester1: [
+      {
+        title: "Machine Learning and Data Mining",
+        link: "https://drive.google.com/drive/folders/1Mi6L0VAmzuLWTvy2I_QTviIJIFSzgqae?usp=drive_link"
+      },
+      {
+        title: "Big Data",
+        link: "https://drive.google.com/drive/folders/1CZtQJQldDA9T3yNnV_QRD6BbnopYjZBU?usp=drive_link"
+      },
+      {
+        title: "Cloud",
+        link: "https://drive.google.com/drive/folders/1PaobsmQUilDQaDeqeZHPZb1mJSPpEEly?usp=drive_link"
+      },
+      {
+        title: "Ethics",
+        link: "https://drive.google.com/drive/folders/1D31py7zZD5mU_XADZwTdHHQjkYLYjnis?usp=drive_link"
+      },
+      {
+        title: "Advanced Artificial Intelligence",
+        link: "https://drive.google.com/drive/folders/1ZU3b4VOQZ_WJyC27lp9G7hbZF0UQX7mK?usp=drive_link"
+      },
+      {
+        title: "Internet of Objects",
+        link: "https://drive.google.com/drive/folders/1kDLV3VLLoXRbedWDix757Zpc02ar-Xbh?usp=drive_link"
+      },
+      {
+        title: "Information Research",
+        link: "https://drive.google.com/drive/folders/1spIyLNjAfps-mtOQJirR2CSctltsiH5C?usp=drive_link"
+      },
+      {
+        title: "Robotics",
+        link: "https://drive.google.com/drive/folders/1AtEhMOC0di2XV-bFa4HXfsubxydH6SKY?usp=drive_link"
+      },
+      {
+        title: "Geographic Information System",
+        link: "https://drive.google.com/drive/folders/1h89HJp3MIBeXlM-lqgWF78iVWLyY8vVM?usp=drive_link"
+      },
+      {
+        title: "Master classes",
+        link: "https://drive.google.com/drive/folders/11p8cgdMIBk0XacASMq949XMukZ58ZmIy?usp=drive_link"
+      }
+    ],
+    semester2: []
+  },
+  SIW: {
+    semester1: [
+      {
+        title: "Machine Learning and Data Mining",
+        link: "https://drive.google.com/drive/folders/1Mi6L0VAmzuLWTvy2I_QTviIJIFSzgqae?usp=drive_link"
+      },
+      {
+        title: "Cloud",
+        link: "https://drive.google.com/drive/folders/1PaobsmQUilDQaDeqeZHPZb1mJSPpEEly?usp=drive_link"
+      },
+      {
+        title: "Ethics",
+        link: "https://drive.google.com/drive/folders/1D31py7zZD5mU_XADZwTdHHQjkYLYjnis?usp=drive_link"
+      },
+      {
+        title: "Internet of Objects",
+        link: "https://drive.google.com/drive/folders/1kDLV3VLLoXRbedWDix757Zpc02ar-Xbh?usp=drive_link"
+      },
+      {
+        title: "Information Research",
+        link: "https://drive.google.com/drive/folders/1spIyLNjAfps-mtOQJirR2CSctltsiH5C?usp=drive_link"
+      },
+      {
+        title: "Geographic Information System",
+        link: "https://drive.google.com/drive/folders/1h89HJp3MIBeXlM-lqgWF78iVWLyY8vVM?usp=drive_link"
+      }
+    ],
+    semester2: []
+  },
+  IASD: {
+    semester1: [
+      {
+        title: "Cloud",
+        link: "https://drive.google.com/drive/folders/1PaobsmQUilDQaDeqeZHPZb1mJSPpEEly?usp=drive_link"
+      },
+      {
+        title: "Computer Vision",
+        link: "https://drive.google.com/drive/folders/1GoJcbjQFWertsD_x1ago7HkN8csLNudE?usp=drive_link"
+      },
+      {
+        title: "Data Visualization",
+        link: "https://drive.google.com/drive/folders/1JGKb8N5_LZ0_8a85EsaNMGJs-TCVEiEW?usp=drive_link"
+      },
+      {
+        title: "Ethics",
+        link: "https://drive.google.com/drive/folders/1D31py7zZD5mU_XADZwTdHHQjkYLYjnis?usp=drive_link"
+      },
+      {
+        title: "Advanced Artificial Intelligence",
+        link: "https://drive.google.com/drive/folders/1Tu9g3SOECxbNieyrJPUQ-1dEmw5d-34N?usp=drive_link"
+      },
+      {
+        title: "Network Science",
+        link: "https://drive.google.com/drive/folders/1tB49gSHPnUglMqLfKFvmR15MFOSi90Ns?usp=drive_link"
+      },
+      {
+        title: "Reinforcement Learning",
+        link: "https://drive.google.com/drive/folders/1J96tJioISXGLTKZa1QCTyLl3MZ8U6rQm?usp=drive_link"
+      },
+      {
+        title: "Robotics",
+        link: "https://drive.google.com/drive/folders/1AtEhMOC0di2XV-bFa4HXfsubxydH6SKY?usp=drive_link"
+      },
+      {
+        title: "Web Semantics",
+        link: "https://drive.google.com/drive/folders/1a5BYlH_QCfAtMW15sbpZo38p8vJ8_Bla?usp=drive_link"
+      }
+    ],
+    semester2: []
+  }
+};
+
+export default fifthYear;
