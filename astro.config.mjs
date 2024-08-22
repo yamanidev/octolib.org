201fc3075ff0fef256793a3a806cea0d5366ace2
yamanidev <elamine.yamani@gmail.com>
2024-08-22 17:31:15 +0100

    Corrected Astro site property + added canonical meta tag

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://octolib.pages.dev/",
+  site: "https://octolib.org/",
   integrations: [tailwind()]
 });
